9c200dd2d7245532f56d57826b2d56c4f2334ad6
manuelja123 <40972971+Manuel151290@users.noreply.github.com>
2024-12-07 20:17:01 -0500

    Envio Cambios

--- TUMI.TIC.ClienteWeb/obj/Debug/net6.0/TUMI.TIC.ClienteWeb.AssemblyInfo.cs
+++ TUMI.TIC.ClienteWeb/obj/Debug/net6.0/TUMI.TIC.ClienteWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TUMI.TIC.ClienteWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127774b911dbf719c3eade2ec87ba21232d03e2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f36656683d93bca831b32049b245c0cb71dc10d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TUMI.TIC.ClienteWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TUMI.TIC.ClienteWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
